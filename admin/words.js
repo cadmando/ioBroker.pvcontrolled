a007ac2b378ef9f5f88d75a71f71a32bae9ac869
dietmar <dietmar@iobroker.home.local>
2019-05-14 10:17:56 +0000

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"pvcontrolled adapter settings": {
+		"en": "Adapter settings for pvcontrolled",
+		"de": "Adaptereinstellungen für pvcontrolled",
+		"ru": "Настройки адаптера для pvcontrolled",
+		"pt": "Configurações do adaptador para pvcontrolled",
+		"nl": "Adapterinstellingen voor pvcontrolled",
+		"fr": "Paramètres d'adaptateur pour pvcontrolled",
+		"it": "Impostazioni dell'adattatore per pvcontrolled",
+		"es": "Ajustes del adaptador para pvcontrolled",
+		"pl": "Ustawienia adaptera dla pvcontrolled",
+		"zh-cn": "pvcontrolled的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
